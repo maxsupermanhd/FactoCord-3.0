89a1face71238c9ef75efcf62622ed44b7e6d022
TechnoStrife <plotnikov7rodion@gmail.com>
2020-06-06 18:51:39 +0300

    Add roles output to help command and FactoCord version (#24)
    * Add roles description to help command
    
    * Add FactoCord version output to version command
    
    - Fix help command roles output
    - Remove debug print

--- commands/utils/version.go
+++ commands/utils/version.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
-	
+
 	"../../support"
 	"github.com/bwmarrin/discordgo"
 )
@@ -13,12 +13,21 @@ var VersionStringUsage = "Usage: $version"
 
 func VersionString(s *discordgo.Session, m *discordgo.MessageCreate, ar string) {
 	cmd := exec.Command(support.Config.Executable, "--version")
-    out, err := cmd.CombinedOutput()
+	out, err := cmd.CombinedOutput()
+	factorioVersion := strings.Fields(string(out))[1]
 	if err != nil {
 		s.ChannelMessageSend(support.Config.FactorioChannelID, fmt.Sprintf("Sorry, there was an error. Error details: %s", err))
 	}
-	s.ChannelMessageSend(support.Config.FactorioChannelID, "Server version: " + strings.Fields(string(out))[1]);
+	res := "Server version: **" + factorioVersion + "**"
+
+	cmd = exec.Command("git", "describe", "--tags")
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		s.ChannelMessageSend(support.Config.FactorioChannelID, fmt.Sprintf("Sorry, there was an error. Error details: %s", err))
+	}
+	factocord := "FactoCord version: **" + string(out) + "**"
+	res += "\n" + factocord
+
+	s.ChannelMessageSend(support.Config.FactorioChannelID, res)
 	return
 }
-
-
