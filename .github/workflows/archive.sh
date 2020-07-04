8a4261a6c84e3ffeb06d2863e48007eead6e0c71
TechnoStrife <plotnikov7rodion@gmail.com>
2020-07-04 14:57:55 +0300

    Add automatic release building
    - Fix for newer discordgo version

---
+++ .github/workflows/archive.sh
@@ -0,0 +1,28 @@
+
+rm -rf ./release
+
+mkdir ./release
+mkdir ./release/FactoCord3
+
+version=`git describe --tags`
+version+=" ("
+version+=`git rev-parse --short HEAD`
+version+=")"
+
+echo "Version: ${version}"
+echo "${version}" > ./release/FactoCord3/.version
+
+cp .envexample control.lua FactoCord-3.0 INSTALL.md LICENSE README.md SECURITY.md ./release/FactoCord3
+
+pushd ./release > /dev/null
+
+pushd ./FactoCord3 > /dev/null
+chmod 664 **
+chmod +x ./FactoCord-3.0
+popd > /dev/null
+
+zip -q ./FactoCord3.zip -r ./FactoCord3 || exit 3
+echo "Created .zip archive"
+tar -czf ./FactoCord3.tar.gz ./FactoCord3 || exit 4
+echo "Created .tar archive"
+popd > /dev/null
